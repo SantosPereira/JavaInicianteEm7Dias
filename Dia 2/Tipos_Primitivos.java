b37669e94a65fd7a9b71c01dea0330248e206fa4
SantosPereira <pedrohenriquelemam@gmail.com>
2021-04-28 23:01:26 -0300

    Create Tipos_Primitivos.java

---
+++ Dia 2/Tipos_Primitivos.java
@@ -0,0 +1,12 @@
+public class Tipos_Primitivos {
+    public static void main(String[] args) {
+        byte Byte = 127;
+        int Inteiro = 1;
+        short Short = 31354;
+        long Long = 1545148116167468642L;
+        float Float = 2.6f;
+        double Double = 3.59;
+        char Caractere = 'C';
+        boolean Booleano = true;
+    }
+}
