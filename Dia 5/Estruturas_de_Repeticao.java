2dd8d09e9cc03a6d85aa6b499ca0083fbcb47f0d
SantosPereira <pedrohenriquelemam@gmail.com>
2021-05-01 23:40:55 -0300

    Update Estruturas_de_Repeticao.java

--- Dia 5/Estruturas_de_Repeticao.java
+++ Dia 5/Estruturas_de_Repeticao.java
@@ -1,5 +1,8 @@
 public class Estruturas_De_Repeticao {
     public static void main(String args[]) {
-        
+        for (int x = 0; x < 100; x++) {
+            System.out.println(x);
+        }
+
     }
 }
