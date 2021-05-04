be2760464a9763d40d38d369d3d1318030e43e24
SantosPereira <pedrohenriquelemam@gmail.com>
2021-05-04 00:04:58 -0300

    Update Exercicio2.java

--- Dia 7/Exercicio2.java
+++ Dia 7/Exercicio2.java
@@ -4,6 +4,10 @@ public class Exercicio2 {
     public static void main(String args[]) {
         Scanner input = new Scanner(System.in);
 
+        String nome = input.next();
+        int idade = input.nextInt();
+
+        System.out.println("Olá, "+nome+". Sua idade é "+idade+", certo?");
         
     }
 }
